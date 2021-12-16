61c093b6a775513928f74da89fb5ec8d1243a277
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-16 06:37:09 -0800

    Cleaned up unused CRA files and code

---
+++ client/CLIENTBUILDING.md
@@ -0,0 +1,1 @@
+# Instructions for working on the client
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// CSS Import
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
