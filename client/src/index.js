7e1b9eaf33741edf0f6a455a917b596ca5a3de92
Lance Turbes <lanceturbes@users.noreply.github.com>
2021-12-18 09:49:33 -0800

    installed react-router; added router to `index.js`

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // CSS Import
 import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
