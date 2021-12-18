3deb9bff445d9e2823fbd2e57db88eec534fb058
Lance Turbes <lanceturbes@users.noreply.github.com>
2021-12-17 21:04:26 -0800

    added login form schema

---
+++ client/src/schemas/login-form-schema.js
@@ -0,0 +1,14 @@
+import * as yup from "yup"
+
+const loginFormSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required("username is required"),
+  password: yup
+    .string()
+    .trim()
+    .required("password is required")
+})
+
+export default loginFormSchema
