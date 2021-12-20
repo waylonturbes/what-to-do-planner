d30bba8dc6c45c5a1458b0367f8dc7f7f1f0c6cd
Lance Turbes <lanceturbes@users.noreply.github.com>
2021-12-19 16:31:15 -0800

    adjusted server listen message

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require("dotenv").config()
-
 const server = require('./api/server.js')
 
 const PORT = process.env.PORT || 9000
 
 server.listen(PORT, () => {
-  console.log(`***\n Server running on port ${PORT}... ***\n`)
+  console.log(`Server running on port ${PORT}...`)
 })
