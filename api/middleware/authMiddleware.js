35b2aef84ceb3dffe4f7dd7bb91e58da24104843
Lance Turbes <lanceturbes@users.noreply.github.com>
2021-12-21 11:57:22 -0800

    added user(name) checking MW

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -1,3 +1,4 @@
+const User = require("../models/usersModel")
 const { loginSchema, registerSchema } = require("../schemas/authSchema")
 
 async function validateLoginInput(req, res, next) {
@@ -6,7 +7,7 @@ async function validateLoginInput(req, res, next) {
     next()
   } catch (err) {
     next({
-      status: 401,
+      status: 400,
       message: err.errors[0]
     })
   }
@@ -18,13 +19,44 @@ async function validateRegisterInput(req, res, next) {
     next()
   } catch (err) {
     next({
-      status: 401,
+      status: 400,
       message: err.errors[0]
     })
   }
 }
 
+async function checkUserExists(req, res, next) {
+  const { username } = req.body
+  const [existingUser] = await User.getBy({ username })
+
+  if (existingUser === undefined) {
+    return next({
+      status: 404,
+      message: "user does not exist"
+    })
+  }
+
+  req._user = existingUser
+  next()
+}
+
+async function checkUsernameTaken(req, res, next) {
+  const { username } = req.body
+  const [existingUser] = await User.getBy({ username })
+
+  if (existingUser) {
+    return next({
+      status: 409,
+      message: "username already taken"
+    })
+  }
+
+  next()
+}
+
 module.exports = {
   validateLoginInput,
-  validateRegisterInput
+  validateRegisterInput,
+  checkUserExists,
+  checkUsernameTaken
 }
