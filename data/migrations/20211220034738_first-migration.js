5706e25bbe8afb7fdd306ebdef9cd7eb013bdb18
Lance Turbes <lanceturbes@users.noreply.github.com>
2021-12-21 08:40:48 -0800

    added new constraints to users table 1st migration

--- data/migrations/20211220034738_first-migration.js
+++ data/migrations/20211220034738_first-migration.js
@@ -1,12 +1,18 @@
-
 exports.up = async function (knex) {
   return await knex.schema
     .createTable("users", table => {
       table.increments("user_id")
+        .primary()
       table.string("first_name")
       table.string("last_name")
+      table.string("email")
+        .notNullable()
+        .unique()
       table.string("username")
-      table.string("user_id")
+        .notNullable()
+        .unique()
+      table.string("password")
+        .notNullable()
     })
 };
 
